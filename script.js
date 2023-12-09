f4af0c2a670897c190c1c1fb7e776deef5ef8528
Liam <x00173318@myTUDublin.ie>
2023-12-09 19:59:51 +0000

    update

---
+++ script.js
@@ -0,0 +1,31 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', function () {
+    var contactButton = document.getElementById('contactButton');
+
+    if (contactButton) {
+        contactButton.addEventListener('click', function () {
+            var myModal = new bootstrap.Modal(document.getElementById('contactModal'), {
+                keyboard: false
+            });
+            myModal.show();
+        });
+    }
+
+    document.addEventListener('DOMContentLoaded', function () {
+        var gradesCollapse = new bootstrap.Collapse(document.getElementById('gradesCollapse'), {
+            toggle: false
+        });
+    
+        document.getElementById('viewGradesButton').addEventListener('click', function () {
+            gradesCollapse.toggle();
+        });
+    });
+
+  
+
+
+
+});
+
+
